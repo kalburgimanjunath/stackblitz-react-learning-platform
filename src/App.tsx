8124ff928e1e943b61185ee729ef26d2fc54988c
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-07-24 18:11:11 +0530

    4 files modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import * as React from 'react';
 import { Header, Footer, Banner } from './components';
 import './style.css';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { SocialShare } from './pages/learning/socialshare/index';
 export default function App() {
   return (
     <div>
-      <Header />
-      <Banner />
-      <Footer />
+      <Router>
+        <Header />
+        <Banner />
+        <Routes>
+          <Route path={`/learning`} element={<SocialShare />}></Route>
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
--- src/pages/learning/socialshare/index.js
+++ src/pages/learning/socialshare/index.js
@@ -1,3 +1,81 @@
-export default function index() {
-  return <div>Social Share</div>;
+import React, { useState } from 'react';
+function index() {
+  return (
+    <div>
+      <h1>Social Share</h1>
+    </div>
+  );
 }
+const PostCard = ({ data }) => {
+  return (
+    <div>
+      <div></div>
+      <div>
+        <div>{data.title}</div>
+        <div>{data.description}</div>
+        <div>
+          {data.tags &&
+            data.tags.map((item) => {
+              return <span>{item}</span>;
+            })}
+        </div>
+      </div>
+    </div>
+  );
+};
+function SocialShare() {
+  const [title, SetTitle] = useState();
+  const [description, SetDescription] = useState();
+  const [image, SetImage] = useState();
+  const [tags, SetTags] = useState([]);
+  const data = {
+    title: 'Post title',
+    description: 'Post description',
+    image: '',
+    tags: ['posts', 'description'],
+  };
+  return (
+    <div className="social">
+      <div className="social-form">
+        {/* <h1>Social Share</h1> */}
+        <div>
+          <div>
+            <div>Title</div>
+            <div>
+              <input type="" />
+            </div>
+          </div>
+          <div>
+            <div>Description</div>
+            <div>
+              <input type="" />
+            </div>
+          </div>
+          <div>
+            <div>Image</div>
+            <div>
+              <input type="" />
+            </div>
+          </div>
+          <div>
+            <div>Tags</div>
+            <div>
+              <input type="" />
+            </div>
+          </div>
+          <div className="button-footer">
+            <div>
+              <button type="button">Save</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="social-preview">
+        <h3>Preview</h3>
+        <PostCard data={data} />
+      </div>
+    </div>
+  );
+}
+
+export { SocialShare };
