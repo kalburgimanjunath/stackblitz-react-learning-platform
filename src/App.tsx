183c9dc992ceed5edcc69ecba12343ef1ee7b8fc
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-07-25 11:45:32 +0530

    1 file modified, 1 added

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import { Header, Footer, Banner } from './components';
 import './style.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { SocialShare } from './pages/learning/socialshare/index';
+import Platform from './platform/';
 export default function App() {
   return (
     <div>
       <Router>
         {/* <Header /> */}
         {/* <Banner /> */}
+
         <Routes>
-          <Route path={`/`} element={<SocialShare />}></Route>
+          <Route exact path={`/`} element={<SocialShare />}></Route>
+          <Route path={`/platform`} element={<Platform />}></Route>
         </Routes>
         {/* <Footer /> */}
       </Router>
---
+++ src/platform/index.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+export default function Platform() {
+  return <div>Platform</div>;
+}
