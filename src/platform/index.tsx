d968f1d4fd8747df568274571782ab5a394ccc14
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-07-25 11:50:46 +0530

    2 files modified

--- src/platform/index.tsx
+++ src/platform/index.tsx
@@ -1,4 +1,25 @@
 import React from 'react';
+const Header = () => {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+      <ul>
+        <li>Home</li>
+        <li>Lesson</li>
+        <li>Lesson</li>
+      </ul>
+      <ul>
+        <li>Hide Solution</li>
+        <li>Hide Preview</li>
+        <li>Slides</li>
+        <li>Feedback</li>
+      </ul>
+    </div>
+  );
+};
 export default function Platform() {
-  return <div>Platform</div>;
+  return (
+    <div className="platform">
+      <Header />
+    </div>
+  );
 }
