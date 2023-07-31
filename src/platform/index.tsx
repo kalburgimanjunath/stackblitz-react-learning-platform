3023dcbd8817aa91788b75c0e0637cdd4d0fdff8
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-07-31 18:40:17 +0530

    2 files modified

--- src/platform/index.tsx
+++ src/platform/index.tsx
@@ -14,29 +14,51 @@ const Header = ({ breadcrum, activeItem, showSolution, showPreview }) => {
   }
   return (
     <div className="flex bg-pink-200 text-blue-500 cursor-pointer p-2 justify-between">
-      <div>
-        {breadcrum &&
-          breadcrum.map((item) => {
-            return (
-              <li className={activeItem == item.title ? 'active' : ''}>
-                {item.title}
-              </li>
-            );
-          })}
+      <div className="fixed inset-0 overflow-hidden">
+        <div className="absolute inset-0 overflow-hidden">
+          <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+            <div className="pointer-events-auto relative w-screen max-w-md">
+              <div className="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl">
+                <div className="px-4 sm:px-6">
+                  <h2
+                    className="text-base font-semibold leading-6 text-gray-900"
+                    id="slide-over-title"
+                  >
+                    Panel title
+                  </h2>
+                </div>
+                <div className="relative mt-6 flex-1 px-4 sm:px-6">
+                  <ol>
+                    {breadcrum &&
+                      breadcrum.map((item) => {
+                        return (
+                          <li
+                            className={activeItem == item.title ? 'active' : ''}
+                          >
+                            {item.title}
+                          </li>
+                        );
+                      })}
+                  </ol>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <SideOverlays />
+      {/* <SideOverlays /> */}
       <ul>
         <li>Home</li>
         {/* <li>Lesson</li>
         <li>Lesson</li> */}
-        {breadcrum &&
+        {/* {breadcrum &&
           breadcrum.map((item) => {
             return (
               <li className={activeItem == item.title ? 'active' : ''}>
                 {item.title}
               </li>
             );
-          })}
+          })} */}
       </ul>
       <ul>
         <li
